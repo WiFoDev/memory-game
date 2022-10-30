059f2c4a99e1298a8ca4002724c2213605896d63
WiFoDev <lou81804@gmail.com>
2022-10-29 19:57:09 -0500

    add main and footer layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,22 @@ import type {NextPage} from "next";
 
 const Home: NextPage = () => {
   return (
-    <header className="sticky top-0 z-20 w-full">
-      <div className="flex pointer-events-none h-16 mx-auto max-w-[90rem] items-center justify-end gap-2 pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
-        <h1 className="mr-auto text-xl">Memory Game</h1>
-        <p className="text-lg">Attemps: 0</p>
-      </div>
-    </header>
+    <>
+      <header className="sticky top-0 z-20 w-full">
+        <div className="flex pointer-events-none h-16 mx-auto max-w-[90rem] items-center justify-end gap-2 pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
+          <h1 className="mr-auto text-xl">Memory Game</h1>
+          <p className="text-lg">Attemps: 0</p>
+        </div>
+      </header>
+      <main className="relative flex flex-col items-center justify-center w-full h-full">
+        <section>Welcome</section>
+      </main>
+      <footer className="relative pb-[env(safe-area-inset-bottom)]">
+        <div className="mx-auto max-w-[90rem] py-12 flex justify-center pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
+          Here is the footer section
+        </div>
+      </footer>
+    </>
   );
 };
 
